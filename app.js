6ad877e40b24cf41e927bb3b7daf6a75b1302c84
ParsaHaghighatgoo <time.phgg@gmail.com>
2023-10-13 04:39:47 +0330

    end of 12

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-//DEFINE VARS
+//DEFINE GLOBAL VARS
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+let posts = [];
 
 
 //SET APP NAME FOR EXPRESS
@@ -25,14 +26,45 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
-
 //HOME ROUTE PAGE RESPONSE(HOME.EJS)
 app.get("/", function(req, res){
-  res.render("home");
+  res.render("home",
+  {
+    homeStartingContent: homeStartingContent ,
+    posts: posts
+  });
 });
 
-//git test
+//CONTACT ROUTE PAGE RESPONSE(CONTACT.EJS)
+app.get("/contact", function(req, res){
+  res.render("contact",{contactContent: contactContent});
+});
+
+//ABOUT ROUTE PAGE RESPONSE(ABOUT.EJS)
+app.get("/about", function(req, res){
+  res.render("about",{aboutContent: aboutContent});
+  
+});
+
+//COMPOSE ROUTE PAGE RESPONSE(ABOUT.EJS)
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+//COMPOSE POST REQ AND RES
+app.post("/compose",function(req, res){
+
+  let composeInput = {
+    composeTitle: req.body.composeTitle,
+    composePost: req.body.composePost
+  };
+  
+  posts.push(composeInput);
+
+  res.redirect("/");
+});
+
+
 
 
 //PORT
